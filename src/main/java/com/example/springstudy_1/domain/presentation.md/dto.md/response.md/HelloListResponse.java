6954c2462b6a3cf91c344cb3f8aa42033e664a85
신용철 <163130418+yongyong47@users.noreply.github.com>
2024-07-28 22:26:50 +0900

    Update HelloListResponse.java

--- src/main/java/com/example/springstudy_1/domain/presentation.md/dto.md/response.md/HelloListResponse.java
+++ src/main/java/com/example/springstudy_1/domain/presentation.md/dto.md/response.md/HelloListResponse.java
@@ -1,1 +1,11 @@
+package com.example.springstudy_1.domain.presentation.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+@Getter
+@AllArgsConstructor
+public class HelloListResponse {
+    private final List<HelloElement> helloList;
+}
