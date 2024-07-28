c86388c51105fa08bbbc3fbe17f39fdcad8e5beb
신용철 <163130418+yongyong47@users.noreply.github.com>
2024-07-28 22:27:40 +0900

    Update HelloNameResponse.java

--- src/main/java/com/example/springstudy_1/domain/presentation.md/dto.md/response.md/HelloNameResponse.java
+++ src/main/java/com/example/springstudy_1/domain/presentation.md/dto.md/response.md/HelloNameResponse.java
@@ -1,1 +1,11 @@
+package com.example.springstudy_1.domain.presentation.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class HelloNameResponse {
+    private final int age;
+    private final String name;
+}
