be470ae3dee44a0e4440b025388f0ede5b2857c7
zweadfx08 <163130418+zweadfx08@users.noreply.github.com>
2024-08-20 17:23:41 +0900

    Hello CRUD 삭제

--- HelloCRUD/src/main/java/com/example/hellocrud/domain/repository/HelloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hellocrud.domain.repository;
-
-import com.example.hellocrud.domain.entity.Hello;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HelloRepository extends CrudRepository<Hello, Long> {
-}
--- HelloCRUD/src/main/java/com/example/hellocrud/domain/service/ReadHelloService.java
+++
@@ -1,4 +0,0 @@
-package com.example.hellocrud.domain.service;
-
-public class ReadHelloService {
-}
